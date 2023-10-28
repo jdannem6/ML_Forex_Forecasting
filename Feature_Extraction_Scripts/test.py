79fbb9093f5176285e01238210252633975fd2ce
jdannem6 <jdannem6@kent.edu>
2023-10-28 12:40:38 -0400

    Removed test fule

--- Feature_Extraction_Scripts/test.py
+++
@@ -1,9 +0,0 @@
-def test_func(function_to_pass):
-    function_to_pass(2)
-    print("It worked")
-
-def callee_function(x):
-    print("whipee")
-if __name__ == "__main__":
-    test_func(callee_function)
-    
